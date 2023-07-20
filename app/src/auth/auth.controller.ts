e83147432948814fb5e323ec71d62f3b3a1bfb9f
Alejandro Hernandez ontoria <ahernand@c1r8s1.42madrid.com>
2023-07-20 20:34:51 +0200

    prisma added, mid 52 of video

---
+++ app/src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import {Controller, Post, Req} from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) { }
+    
+    @Post('signup')
+    signup(@Body() dto: any) {
+        return this.authService.signup();
+    }
+
+    @Post('signin')
+    signin() {
+        return this.authService.signin();
+    }
+}
