bc0dd3ecb8a1b39f5cbf8489b7e64f9c8206cd5d
Alejandro Hernandez ontoria <ahernand@c1r8s6.42madrid.com>
2023-08-03 18:35:00 +0200

    1:35

--- app/src/prisma/prisma.service.ts
+++ app/src/prisma/prisma.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
+    constructor(config: ConfigService) {
         super({
             datasources: {
                 db: {
-                    url : "postgresql://user:secret@postgres:5432/db_nestjs?schema=public"
-                }
-            }
-        })
+                    url : config.get('DATABASE_URL'),
+                },
+            },
+        });
+        console.log(config.get('DATABASE_URL'));
     }
 }
