3a354b6011011c7b97cf57e45e60542c57e3162b
PotatomixS <45759240+PotatomixS@users.noreply.github.com>
2023-09-29 20:14:17 +0200

    Componentes

---
+++ app-frontend/src/app/pong-page/pong-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pong-page',
+  templateUrl: './pong-page.component.html',
+  styleUrls: ['./pong-page.component.css']
+})
+export class PongPageComponent {
+
+}
--- app-frontend/src/app/profile-page/profile-page.component.ts
+++ app-frontend/src/app/profile-page/profile-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-page',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile-page.component.css']
 })
 export class ProfilePageComponent {
-
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
 }
