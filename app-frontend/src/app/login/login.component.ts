e6e658e2173b535b379affdbc69c8940bbbecd10
rgirondo <rgirondo@pop-os.localdomain>
2023-10-13 15:31:50 +0200

    2FA completado

--- app-frontend/src/app/login/login.component.ts
+++ app-frontend/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, SimpleChange, SimpleChanges } from '@angular/core';
+import { Component, ElementRef, Input, SimpleChange, SimpleChanges } from '@angular/core';
 import { SharedService } from '../shared.service';
 import { FormGroup, FormControl } from '@angular/forms';
+import { count } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -18,7 +19,6 @@ export class LoginComponent
     Code5: new FormControl(''),
     Code6: new FormControl('')
   });
-
   
   constructor(public ss: SharedService)
   {
@@ -33,19 +33,89 @@ export class LoginComponent
     return false;
   }
 
-  ngOnInit(changes: SimpleChanges)
+  ngOnInit()
   {
-    this.onSubmit();
+    this.CodeForm.valueChanges.subscribe(values =>
+      {
+        this.onSubmit(values);
+      });
   }
   
-  onSubmit()
+  onSubmit(values: any)
   {
-      console.log((this.CodeForm.value).Code1);
-      if (this.CodeForm.value.Code1 && this.CodeForm.value.Code2 &&
-        this.CodeForm.value.Code3 && this.CodeForm.value.Code4 &&
-        this.CodeForm.value.Code5 && this.CodeForm.value.Code6)
+    const Code: String = values.Code1 + values.Code2 + values.Code3 + values.Code4 + values.Code5 + values.Code6;
+    if (values.Code1 && values.Code2 &&
+      values.Code3 && values.Code4 &&
+      values.Code5 && values.Code6)
       {
-
+        document.getElementById("code1")?.blur();
+        if (Code == "123456")
+        {
+          console.log("Success");
+        }
+        else
+        {
+          console.log("Failure");
+          this.CodeForm.reset();
+        }
+        console.log(Code);
       }
+    }
+
+  onKeyDown(key: KeyboardEvent)
+  {
+    var values_list: Array<string | null | undefined>;
+    var id_list: Array<string>;
+  
+    values_list = [this.CodeForm.value.Code1, this.CodeForm.value.Code2,
+                   this.CodeForm.value.Code3, this.CodeForm.value.Code4,
+                   this.CodeForm.value.Code5, this.CodeForm.value.Code6];
+    id_list = ["code1", "code2", "code3",
+               "code4", "code5", "code6"];
+    if (key.key != "Backspace")
+      return;
+    for(var it = 0; it < 6; it++)
+    {
+      if (!values_list[it])
+      {
+        document.getElementById(id_list[it - 1])?.focus();
+        return;
+      }
+    }
+    document.getElementById(id_list[5])?.focus();
+  }
+  
+  onInput(Input: InputEventInit)
+  {
+    var values_list: Array<string | null | undefined>;
+    var id_list: Array<string>;
+  
+    values_list = [this.CodeForm.value.Code1, this.CodeForm.value.Code2,
+                   this.CodeForm.value.Code3, this.CodeForm.value.Code4,
+                   this.CodeForm.value.Code5, this.CodeForm.value.Code6];
+    id_list = ["code1", "code2", "code3",
+               "code4", "code5", "code6"];
+    for(var it = 0; it < 6; it++)
+    {
+      if (!values_list[it])
+      {
+        document.getElementById(id_list[it])?.focus();
+        return;
+      }
+    }
+    document.getElementById(id_list[5])?.focus();
+  }
+
+  onPaste(paste: ClipboardEvent)
+  {
+    const Code: String = String(paste.clipboardData?.getData('text'));
+    this.CodeForm.setValue({
+      Code1: Code[0],
+      Code2: Code[1],
+      Code3: Code[2],
+      Code4: Code[3],
+      Code5: Code[4],
+      Code6: Code[5],
+    });
   }
 }
