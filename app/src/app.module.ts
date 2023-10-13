f44333f061abf55741d964ca69eeda2a5dcabff1
Alejandro Hernandez ontoria <ahernand@c2r11s2.42madrid.com>
2023-10-13 20:53:44 +0200

    Email hace todo

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -7,6 +7,8 @@ import { ConfigModule } from '@nestjs/config';
 //estos estan de forma temporal
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { ChatModule } from './chat/chat.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports:  [
@@ -17,6 +19,8 @@ import { AppController } from './app.controller';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ChatModule,
+    MessagesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ app/src/chat/gateway/chat/chat.gateway.ts
@@ -0,0 +1,13 @@
+import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+
+@WebSocketGateway()
+export class ChatGateway
+{ 
+	@SubscribeMessage('message')
+	handleMessage(client: any, payload: any): string
+	{
+		return 'Hello world!';
+	}
+}
+
+ 
