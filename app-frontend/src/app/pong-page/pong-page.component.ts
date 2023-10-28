3fbc86e07bcf2aa28936f432b664d870b3ff03f8
rgirondo <rgirondo@pop-os.localdomain>
2023-10-28 20:53:34 +0200

    grandes avances en el pong

--- app-frontend/src/app/pong-page/pong-page.component.ts
+++ app-frontend/src/app/pong-page/pong-page.component.ts
@@ -1,10 +1,145 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-pong-page',
   templateUrl: './pong-page.component.html',
   styleUrls: ['./pong-page.component.css']
 })
-export class PongPageComponent {
+export class PongPageComponent implements OnInit
+{
+
+  @ViewChild("game")  
+  private gameCanvas: ElementRef;
+  
+  
+  private canvasElement: HTMLElement | null;
+  private context: any;
+  private drawNumbersArray: ((x: number, y: number) => void)[];
+  
+  constructor()
+  {
+    this.drawNumbersArray  = [this.draw0.bind(this), this.draw1.bind(this),
+       this.draw2.bind(this), this.draw3.bind(this), this.draw4.bind(this),
+        this.draw5.bind(this), this.draw6.bind(this), this.draw7.bind(this),
+         this.draw8.bind(this), this.draw9.bind(this)];
+  }
+
+  ngOnInit()
+  {
+    
+  }
+
+  ngAfterViewInit()
+  {
+    this.canvasElement = document.getElementById("cv");
+    this.context = this.gameCanvas.nativeElement.getContext("2d");
+    this.context.fillStyle = "white";
+    this.context.clearRect(
+      0,
+      0,
+      this.gameCanvas.nativeElement.width,
+      this.gameCanvas.nativeElement.height
+    );
+    this.context.fillRect(15, 405, 15, 70);
+    this.context.fillRect(1240, 405, 15, 70);
+    for(var i = 0; i < 960; i += 24)
+    {
+      this.context.fillRect(635, i, 5, 10);
+    }
+    this.drawPoints(8, 9);
+  }
+
+  drawPoints(points1: number, points2: number)
+  {
+    if (points1 < 10)
+      this.drawNumbersArray[points1](320, 40);
+    else if (points1 >= 10)
+    {
+      
+    }
+    if (points2 < 10)
+      this.drawNumbersArray[points2](960, 40);
+    else if (points2 >= 10)
+    {
+
+    }
+  }
+
+  draw0(x: number, y: number)
+  {
+    this.context.fillRect(x, y, 40, 10);
+    this.context.fillRect(x, y + 70, 40, 10);
+    this.context.fillRect(x, y, 10, 80);
+    this.context.fillRect(x + 30, y, 10, 80);
+  }
+
+  draw1(x: number, y: number)
+  {
+    this.context.fillRect(x + 30, y, 10, 80);
+  }
+
+  draw2(x: number, y: number)
+  {
+    this.context.fillRect(x, y, 40, 10);
+    this.context.fillRect(x, y + 30, 40, 10);
+    this.context.fillRect(x, y + 70, 40, 10);
+    this.context.fillRect(x + 30, y, 10, 30);
+    this.context.fillRect(x, y + 30, 10, 50);
+  }
+
+  draw3(x: number, y: number)
+  {
+    this.context.fillRect(x, y, 40, 10);
+    this.context.fillRect(x, y + 30, 40, 10);
+    this.context.fillRect(x, y + 70, 40, 10);
+    this.context.fillRect(x + 30, y, 10, 80);
+  }
+
+  draw4(x: number, y: number)
+  {
+    this.context.fillRect(x, y + 30, 40, 10);
+    this.context.fillRect(x, y, 10, 30);
+    this.context.fillRect(x + 30, y, 10, 80);
+  }
+
+  draw5(x: number, y: number)
+  {
+    this.context.fillRect(x, y, 40, 10);
+    this.context.fillRect(x, y + 30, 40, 10);
+    this.context.fillRect(x, y + 70, 40, 10);
+    this.context.fillRect(x, y, 10, 30);
+    this.context.fillRect(x + 30, y + 30, 10, 50);
+  }
+
+  draw6(x: number, y: number)
+  {
+    this.context.fillRect(x, y + 30, 40, 10);
+    this.context.fillRect(x, y + 70, 40, 10);
+    this.context.fillRect(x, y, 10, 80);
+    this.context.fillRect(x + 30, y + 30, 10, 50);
+  }
+
+  draw7(x: number, y: number)
+  {
+    this.context.fillRect(x, y, 40, 10);
+    this.context.fillRect(x + 30, y, 10, 80);
+  }
+
+  draw8(x: number, y: number)
+  {
+    this.context.fillRect(x, y, 40, 10);
+    this.context.fillRect(x, y + 30, 40, 10);
+    this.context.fillRect(x, y + 70, 40, 10);
+    this.context.fillRect(x, y, 10, 80);
+    this.context.fillRect(x + 30, y, 10, 80);
+  }
+
+  draw9(x: number, y: number)
+  {
+    this.context.fillRect(x, y, 40, 10);
+    this.context.fillRect(x, y + 30, 40, 10);
+    this.context.fillRect(x, y, 10, 30);
+    this.context.fillRect(x + 30, y, 10, 80);
+  }
 
 }
--- app-frontend/src/app/services/chat-service/chat.service.ts
+++ app-frontend/src/app/services/chat-service/chat.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-// import * as io from 'socket.io-client';
 import { io } from 'socket.io-client';
 import { Profile, ProfileService } from '../profile-service/profile.service';
-
 import { Observable } from 'rxjs';
 
 @Injectable({
